7ffa36b3a63106fb0085be6fac2287134025712e
Husam Obeidat <husamobe1991@gmail.com>
2023-03-13 11:54:02 +0000

    adding lab 09 local storage branch

--- README.md
+++ README.md
@@ -27,3 +27,23 @@ $$
 2-refactored the render method and made it dynamic NOW "the user input the data using the form ,and the renderData function preview these data inside the web page.
  
 3-added a submit handler to handle the form data and pass it to a constructor.
+
+
+
+                                **lab08** 
+$$
+date:3/13/2023
+
+$$
+
+1- refactoring my code pulled the code from app.js that is related to employee constructor 
+
+2- saved all my employees into local storage 
+
+3- defined new constructor called Department 
+
+4- when accounting.js is loaded the deparment list is populated with constant values for each department and a custom "all department".
+
+5-each department employees list is populated 
+
+6-after that the table is rendered.
--- app.js
+++ app.js
@@ -1,50 +1,15 @@
-'use strict'
+'use strict';
 
-const allEmployees = [];
+
+let allEmployees = [];
 let empForm = document.getElementById("form");
 let empCard = document.getElementById("empCard");
 
 
-
-function Employee(name, department, level, image) {
-    this.id = generateRandomId();
-    this.name = name;
-    this.department = department;
-    this.level = level;
-    this.imgUrl = image;
-    this.salary = this.salaryCalc();
-
-}
-
-
-Employee.prototype.salaryCalc = function () {
-    let salaryLevel = 0;
-    switch (this.level) {
-        case "Junior":
-            salaryLevel = Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
-            break;
-        case "Mid-Senior":
-            salaryLevel = Math.floor(Math.random() * (1500 - 1000 + 1)) + 1000;
-            break;
-        case "Senior":
-            salaryLevel = Math.floor(Math.random() * (2000 - 1500 + 1)) + 1500;
-            break;
-    }
-
-    return Math.floor(salaryLevel * (1 - 0.075));
-}
-
-Employee.prototype.renderData = function () {
-    let imgSection = document.createElement("div");
-    imgSection.innerHTML = `
-    <div class="employeeCard">
-    <img src="${this.imgUrl}" alt="img">
-        <p>Name : ${this.name} - ID : ${this.id} - Department : ${this.department} - Level : ${this.level} - Salary : ${this.salary} </p>
-        </div>`;
-    empCard.appendChild(imgSection);
-}
-
 empForm.addEventListener('submit', submitHandler);
+
+
+
 
 function submitHandler(event) {
     event.preventDefault();
@@ -57,9 +22,11 @@ function submitHandler(event) {
     newEmployee.salaryCalc();
     newEmployee.renderData();
     allEmployees.push(newEmployee);
-}
 
 
-function generateRandomId() {
-    return Math.floor(1000 + Math.random() * 9000);
+    localStorage.setItem('All Employees', JSON.stringify(allEmployees));
 }
+
+initEmployees(true);
+
+
